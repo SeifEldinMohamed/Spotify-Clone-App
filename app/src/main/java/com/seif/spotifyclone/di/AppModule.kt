ea34d25585568a1c873ba7cc5bf9558a7551c6e3
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-05-30 23:43:22 +0200

    Provide Glide Instance in AppModule

---
+++ app/src/main/java/com/seif/spotifyclone/di/AppModule.kt
@@ -0,0 +1,28 @@
+package com.seif.spotifyclone.di
+
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.seif.spotifyclone.R
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+@Module
+@InstallIn(Singleton::class)
+object AppModule {
+
+    @Singleton // to use the same instance over and over again
+    @Provides
+    fun provideGlideInstance( // pass the objects we need to create this instance
+       @ApplicationContext context: Context
+    ) = Glide.with(context).setDefaultRequestOptions(
+        RequestOptions()
+            .placeholder(R.drawable.ic_placholder)
+            .error(R.drawable.ic_placholder)
+            .diskCacheStrategy(DiskCacheStrategy.DATA) // to make sure that our images are cashed with glide
+    )
+}
